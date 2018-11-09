2ab81dd0d0a4709a990138444925ea0ec285da46
Philip Devine <devine.pk@gmail.com>
2018-11-09 10:59:27 -0500

    corrects .env file

--- config/database.php
+++ config/database.php
@@ -63,33 +63,33 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-
-
+        // 
         // 'pgsql' => [
         //     'driver' => 'pgsql',
-        //     'host' => $dbhost,
-        //     'port' => $dbport,
-        //     'database' => $dbname,
-        //     'username' => $dbuser,
-        //     'password' => $dbpass,
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
         //     'charset' => 'utf8',
         //     'prefix' => '',
         //     'prefix_indexes' => true,
         //     'schema' => 'public',
         //     'sslmode' => 'prefer',
+
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $dbhost,
+            'port' => $dbport,
+            'database' => $dbname,
+            'username' => $dbuser,
+            'password' => $dbpass,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
 
         ],
 
