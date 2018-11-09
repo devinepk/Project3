a467e09cc4686b0ed59b81a12ada2b2aa71e9179
Philip Devine <devine.pk@gmail.com>
2018-11-09 10:10:46 -0500

    adds placeholder text in AppServiceProvider for secure https request.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // // if(evn('APP_ENV') == 'production') {
+        // //   \URL::forceScheme('https');
+        // }
     }
 
     /**
